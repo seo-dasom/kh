867ad23cd92829f1260947e4cd8169a7d4e9eb82
seo-dasom <74850449+seo-dasom@users.noreply.github.com>
2020-12-17 00:00:20 +0900

    kh_201216

---
+++ ControlPractice.java
@@ -0,0 +1,372 @@
+package com.kh.practice.chap01;
+
+import java.util.Scanner;
+
+public class ControlPractice {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void practice1() {
+		// 조건문_1번 문제
+		int num;
+		
+		System.out.println("1. 입력");
+		System.out.println("2. 수정");
+		System.out.println("3. 조회");
+		System.out.println("4. 삭제");
+		System.out.println("7. 종료");
+		System.out.println("메뉴 번호를 입력하세요 : ");
+		
+		num = sc.nextInt();
+		
+//		if (num == 1) {
+//			System.out.println("입력 메뉴입니다.");
+//		} else if(num == 2) {
+//			System.out.println("수정 메뉴입니다.");
+//		} else if (num == 3) {
+//			System.out.println("조회 메뉴입니다.");
+//		} else if (num == 4) {
+//			System.out.println("삭제 메뉴입니다.");
+//		} else if (num == 7) {
+//			System.out.println("프로그램이 종료됩니다.");
+//		} else {
+//			System.out.println("잘못된 메뉴 번호 입니다.");
+//		}
+		
+		switch(num) {
+		case 1:
+			System.out.println("입력 메뉴입니다."); break;
+		case 2:
+			System.out.println("수정 메뉴입니다."); break;
+		case 3:
+			System.out.println("조회 메뉴입니다."); break;
+		case 4:
+			System.out.println("삭제 메뉴입니다."); break;
+		case 7:
+			System.out.println("프로그램이 종료됩니다."); break;
+		default:
+			System.out.println("잘못된 메뉴 번호 입니다.");
+		}
+	}
+	
+	public static void practice2() {
+		// 2번 문제
+		int num;
+		
+		System.out.print("숫자를 한 개 입력하세요 : ");
+		num = sc.nextInt();
+		
+//		if(num > 0) {
+//			if(num % 2 == 0) {
+//				System.out.println("짝수다.");
+//			} else {
+//				System.out.println("홀수다.");
+//			} 
+//		} else {
+//			System.out.println("양수만 입력해주세요.");
+//		}
+		
+		if(num > 0) {
+			switch(num % 2) {
+			case 0:
+				System.out.println("짝수다."); break;
+			case 1:
+				System.out.println("홀수다."); break;
+			}
+		} else {
+			System.out.println("양수만 입력해주세요.");
+		}
+	}
+	
+	public static void practice3() {
+		int x, y, z, tot;
+		double avg;
+
+		System.out.print("국어점수 : ");
+		x = sc.nextInt();
+		
+		System.out.print("영어점수 : ");
+		y = sc.nextInt();
+		
+		System.out.print("수학점수 : ");
+		z = sc.nextInt();
+		
+		tot = x + y + z;
+		avg = tot / 3.0;
+
+		if (x >= 40 && y >= 40 && z >= 40 && avg >=60) {
+			System.out.printf("국어 : %d 점\n", x);
+			System.out.printf("영어 : %d 점\n", y);
+			System.out.printf("수학 : %d 점\n", z);
+			System.out.printf("합계 : %d 점\n", tot);
+			System.out.printf("평균 : %.2f 점\n", avg);
+			System.out.println("축하합니다. 합격입니다!");
+		} else {
+			System.out.println("불합격입니다.");
+		}
+	}
+	public static void practice4() {
+		
+		int month = 0;
+		String season;
+		
+		System.out.print("1~12 사이의 정수 입력 : ");
+		month = sc.nextInt();
+		
+		switch(month) {
+		
+		case 1: case 2: case 12:
+			season = "겨울"; break;
+		case 3: case 4: case 5:
+			season = "봄"; break;
+		case 6 : case 7: case 8:
+			season = "여름"; break;
+		case 9: case 10: case 11:
+			season = "가을"; break;
+		default:
+			season = "잘못 입력된 달입니다.";
+		}
+		
+		System.out.printf("%d월은 %s입니다.", month, season);
+	}
+	public static void practice5() {
+		// 5번 문제
+		String id, pw;
+		String id_input, pw_input;
+		
+		id = "myId";   pw = "myPassword12";
+		
+		System.out.print("아이디 입력 : ");
+		id_input = sc.next();
+		
+		System.out.print("패스워드 입력 : ");
+		pw_input = sc.next();
+		
+		if(id.equals(id_input) && pw.equals(pw_input)) {
+			System.out.println("로그인 성공");
+		} else {
+			if(!id.equals(id_input) && !pw.equals(pw_input)) {
+				System.out.println("아이디와 패스워드가 틀렸습니다.");
+			} else if(!id.equals(id_input)){
+				System.out.println("아이디가 틀렸습니다.");
+			} else if(!pw.equals(pw_input)){
+				System.out.println("패스워드가 틀렸습니다.");
+			}
+		}
+	}
+	public static void practice6() {
+		// 6번 문제
+		String perm;
+		
+		System.out.print("회원 등급(관리자, 회원, 비회원) : ");
+		perm = sc.next();
+		
+//		if(perm.equals("관리자")) {
+//			System.out.println("회원관리, 게시글 관리, 게시글 작성, 게시글 조회, 댓글 작성");
+//		} else if(perm.equals("회원")) {
+//			System.out.println("게시글 작성, 게시글 조회, 댓글 작성");
+//		} else if(perm.equals("비회원")) {
+//			System.out.println("게시글 조회");
+//		} else {
+//			System.out.println("잘못된 권한 등급입니다.");
+//		}
+		String res = "";
+		
+		switch(perm) {
+		case "관리자":
+			res += "회원관리, 게시글 관리, ";  //// res = res + "회원관리, 게시글 관리";
+		case "회원":
+			res += "게시글 작성, 댓글 작성, "; // res = res + "게시글 작성, 댓글 작성";
+		case "비회원":
+			res += "게시글 조회";           // res = res + "게시글 조회";
+			break;
+		default:
+			res = "잘못된 권한 등급입니다.";
+		}
+		System.out.println(res);
+	}
+	public static void practice7() {
+		// 7번 문제 BMI = 몸무게 / (키(m)*키(m))
+		double tall, weight, bmi;
+		String res;
+		
+		System.out.print("키(cm)를 입력해주세요 : ");
+		tall = sc.nextDouble();
+		tall /= 100;      // tall = tall / 100;
+		
+		System.out.print("몸무게(kg)를 입력해주세요 : ");
+		weight = sc.nextDouble();
+		
+		bmi = weight / (tall * tall);
+		
+//		if(bmi < 18.5) {
+//			res = "저체중";
+//		} else if(bmi >= 18.5 && bmi < 23) {
+//			res = "정상체중";
+//		} else if(bmi >= 23 && bmi < 25) {
+//			res = "과체중";
+//		} else if(bmi >= 25 && bmi < 30) {
+//			res = "비만";
+//		} else if(bmi >= 30) {
+//			res = "고도비만";
+//		} else {
+//			res = "잘못된 입력값이 있습니다.";
+//		}
+
+		if(bmi < 18.5) {
+			res = "저체중";
+		} else if(bmi< 23) {
+			res = "정상체중";
+		} else if(bmi< 25) {
+			res = "과체중";
+		} else if(bmi< 30) {
+			res = "비만";
+		} else {
+			res = "고도비만";
+		}
+		
+		System.out.printf("BMI 지수 : %.2f\n", bmi);
+		System.out.println(res);
+	}
+	public static void practice8() {
+		// 8번 문제
+		int a, b;
+		int iRes = 0;   double dRes = 0.0;
+		char op, flag;  // 계산 결과가 저장된 자료형의 종류를 구분하기 위한 플래그, 'i', 'd'
+		
+		System.out.print("피연산자 1 : ");
+		a = sc.nextInt();
+		
+		System.out.print("피연산자 2 : ");
+		b = sc.nextInt();
+		
+		System.out.print("연산자(+,-,*,/,%) : ");
+		op = sc.next().charAt(0);
+		
+		if( a > 0 && b > 0) {
+		switch(op) {
+		case '+' :
+			iRes = a + b;  flag = 'i';
+//			System.out.printf("%d + %d = %d", a, b, a + b);
+			break;
+		case '-' :
+			iRes = a - b;  flag = 'i';
+//			System.out.printf("%d - %d = %d", a, b, a - b);
+			break;
+		case '*' :
+			iRes = a * b;  flag = 'i';
+//			System.out.printf("%d * %d = %d", a, b, a * b);
+			break;
+		case '/' :
+			dRes = (double)a / b;  flag = 'd';
+//			System.out.printf("%d / %d = %d", a, b,(double)a / b);
+			break;
+		case '%' :
+			iRes = a % b;  flag = 'i';
+//			System.out.printf("%d %% %d = %d", a, b, a % b);
+			break;
+		default:
+			flag = 'e';
+			System.out.printf("잘못 입력하셨습니다. 프로그램을 종료합니다.");
+//			System.out.println("잘못 입력하셨습니다. 프로그램을 종료합니다.");
+		   }
+		if (flag == 'i') {
+			System.out.printf("%d %c %d = %d", a, op, b, iRes);
+		} else if (flag == 'd') {
+			System.out.printf("%d %c %d = %f", a, op, b, dRes);
+		}
+		}
+	}
+	public static void practice9() {
+		// 9번 문제
+		int mid, fin, hw, attend;
+		double m, f, h, a, tot;
+		
+		System.out.print("중간고사 점수 : ");
+		mid = sc.nextInt();
+		System.out.print("기말고사 점수 : ");
+		fin = sc.nextInt();
+		System.out.print("과제 점수 : ");
+		hw = sc.nextInt();
+		System.out.print("출석 회수 : ");
+		attend = sc.nextInt();
+		
+		m = mid * 0.2;
+		f = fin * 0.3;
+		h = hw * 0.3;
+		a = attend * 0.2;
+		tot = m + f + h + a;
+		
+		System.out.println("=================결과=================");
+		System.out.printf("중간고사(20) : %.1f\n", m);
+		System.out.printf("기말고사(30) : %.1f\n", f);
+		System.out.printf("과제점수(30) : %.1f\n", h);
+		System.out.printf("출석점수(20) : %.1f\n", a);
+		System.out.printf("총점 : %.1f", tot);
+	}
+	public static void practice10() {
+		// 10번 문제
+		char select;
+		
+		System.out.println("실행할 기능을 선택하세요.");
+		System.out.println("1. 메뉴 출력");
+		System.out.println("2. 짝수/홀수");
+		System.out.println("3. 합격/불합격");
+		System.out.println("4. 계절");
+		System.out.println("5. 로그인");
+		System.out.println("6. 권한 확인");
+		System.out.println("7. BMI");
+		System.out.println("8. 계산기");
+		System.out.println("9. P/F");
+		
+		System.out.print("선택 : ");
+		select = sc.next().charAt(0);
+		
+		switch(select) {
+		case 1 :
+			practice1();
+			break;
+		case 2 :
+			practice2();
+			break;
+		case 3 :
+			practice3();
+			break;
+		case 4 :
+			practice4();
+			break;
+		case 5 :
+			practice5();
+			break;
+		case 6:
+			practice6();
+			break;
+		case 7:
+			practice7();
+			break;
+		case 8:
+			practice8();
+			break;
+		case 9:
+			practice9();
+			break;
+		default:
+			System.out.println("올바른 메뉴를 입력하세요");	
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//practice1();
+		//practice2();
+		//practice3();
+		//practice4();
+		//practice5();
+		//practice6();
+		//practice7();
+		//practice8();
+		//practice9();
+		practice10();
+		sc.close();
+	}
+
+}
--- Sample1.java
+++ Sample1.java
@@ -1,107 +1,128 @@
-package com.kh.exam01;
+package com.kh.exam;
 
+import java.util.Scanner;
 public class Sample1 {
-
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void func1() {
+		/*
+		 * 사용자 입력으로 정수 값을 입력 받은 후 양수/음수 구분하여 출력
+		 */
+		
+		int num;
+		String res;
+		
+		System.out.print("정수 입력 : ");
+		num = sc.nextInt();
+		
+		if(num > 0) {
+			res = "양수다";
+		} else {
+			res = "양수가 아니다";
+		}
+		
+		System.out.println(res);
+	}
+	
+	public static void func2() {
+		int num;
+		String res;
+		
+		System.out.print("정수 입력 : ");
+		num = sc.nextInt();
+		
+//		if(num > 0) {
+//			res = "양수다";
+//		} else if (num == 0) {
+//			res = "0 이다";
+//		} else {
+//			res = "음수다";
+//		}
+		
+		if(num > 0) {
+			res = "양수다";
+		} else {
+			// 0 을 포함한 음수값
+			if(num == 0) {
+				res = "0이다";
+			} else {
+				res = "음수다";
+			}
+		}
+		
+		System.out.println(res);
+	}
+	
+	public static void func3() {
+		String name, gender_name;
+		int lvl, cls, num;
+		char gender;
+		double grade;
+		
+		System.out.print("이름 : ");
+		name = sc.next();
+		
+		System.out.print("학년 : ");
+		lvl = sc.nextInt();
+		
+		System.out.print("반 : ");
+		cls = sc.nextInt();
+		
+		System.out.print("번호 : ");
+		num = sc.nextInt();
+		
+		System.out.print("성별(M/F) : ");
+		gender = sc.next().charAt(0);
+		
+		System.out.print("성적 : ");
+		grade = sc.nextDouble();
+		
+//		if(gender == 'M') {
+//			gender_name = "남학생";
+//		} else if(gender == 'F') {
+//			gender_name = "여학생";
+//		} else {
+//			gender_name = "잘못된 성별 코드";
+//		}
+		
+		switch(gender) {
+		    case'M' :
+		    	gender_name = "남학생"; break;
+		    case'F'	:
+		    	gender_name = "여학생"; break;
+		    default:
+		    	gender_name = "잘못된 성별 코드";
+		}
+		
+		System.out.printf("%d 학년 %d 반 %d 번 %s %s 성적은 %.2f이다.\n", 
+				lvl, cls, num, name, gender_name, grade);
+	}
+	
+	public static void func4() {
+		int age;
+		String res;
+		
+		System.out.print("나이 (1 ~ 120) : ");
+		age = sc.nextInt();
+		
+		if(age > 0 && age <= 120) {
+			if(age <= 13) {
+				res = "어린이";
+			} else if(age > 13 && age <= 19) {
+				res ="청소년";
+			} else {
+				res = "성인";
+			}
+		} else {
+			res = "1 ~ 120 사이의 갑을 입력하세요";
+		}
+		
+		System.out.println(res);
+	}
+	
 	public static void main(String[] args) {
-		boolean b1 = true;
-		boolean b2 = !b1;
-		System.out.print(b1);
-		System.out.print(" " + b2 + "\n");
-		
-		//++ -> +1 증가, -- -> -1 감소
-		int n1 = 10;
-		System.out.println(++n1); // 증가 계산 후 출력(전위)
-		
-		int n2 = 20;
-		System.out.println(n2++); // 출력 후 증가 계산(후위)
-		System.out.println(n2);
-		
-		int n3 = 15;
-		System.out.println(--n3); // 감소 계산 후 출력(전위)
-		
-		int n4 = 25;
-		System.out.println(n4--); //출력 후 감소 계산(후위)
-		System.out.println(n4);
-		
-		// 산술 연산 : +, -, *, /, %(나머지)
-		System.out.println("몫 : " + 10 / 3);
-		System.out.println("나머지 : "+ 10 % 3);
-		System.out.println("나머지가 0 이면 3의 배수 12 % 3 -> " + 12 % 3);
-		System.out.println("나머지가 1 이면 홀수 11 % 2 -> " + 11 % 2);
-		
-		// 비교연산 : ==(같다), !=(다르다), >(왼쪽값이 크다), <(왼쪽값이 작다)
-		//         >=(왼쪽값이 크거나 같다), <=(왼쪽값이 작거나 같다)
-		// 비교연산의 결과는 논리값(true, false)으로 나온다.
-		System.out.println(1 == 1);
-		System.out.println(1 == 2);
-		System.out.println(1 != 2);
-		System.out.println(1 > 2);
-		System.out.println(1 < 2);
-		System.out.println(1 >= 2);
-		System.out.println(1 <= 2);
-		
-		// 논리연산 : 논리값을 사용한 연산
-		//          &&(and 연산) : 두 피연산자의 논리값이 true 일때 true가 된다.
-		//          ||(or 연산) : 두 피연산자 중의 하나의 논리값이 true 일때 true가 된다.
-		System.out.println("true && true -> " + (true && true));
-		System.out.println("true && false -> " + (true && false));
-		System.out.println("false && true -> " + (false && true));
-		System.out.println("false && false -> " + (false && false));
-		
-		System.out.println("true || true -> " + (true || true));
-		System.out.println("true || false -> " + (true || false));
-		System.out.println("false || true -> " + (false || true));
-		System.out.println("false || false -> " + (false || false));
-		
-		// 비트연산 : 2진수 값에 대한 연산을 수행
-		//         &(bit and 연산) : 두 비트의 값이 1일때 1이 된다.
-		//         |(bit or 연산) : 두 비트의 값 중 하나의 비트가 1일때 1이 된다.
-		//         ^(bit xor 연산) : 두 비트의 값이 같으면 0, 다르면 1이 된다.
-		//                         베타적 논리합
-		//         >>(쉬프트 연산) : 오른쪽 방향으로 비트 이동(1비트 이동시 2로 나눈 효과)
-		//         <<(쉬프트 연산) : 왼쪽 방향으로 비트 이동(1비트 이동시 2로 곱한 효과)
-		//         >>>(쉬프트 연산) : 오른쪽 방향으로 비트 이동(부호비트에 대한 고려 없이 0을 채움)
-		// 10 & 6 -> 1010 & 0110 -> 0010 -> 2
-		// 10 | 6 -> 1010 | 0110 -> 1110 -> 14
-		// 10 | 6 -> 1010 ^ 0110 -> 1100 -> 12
-		System.out.println(10 & 6);
-		System.out.println(10 | 6);
-		System.out.println(10 ^ 6);
-		System.out.println(-10 | -6);
-		System.out.println(-10 & -6);
-		// 1010 -> 101(5) -> 10(2)
-		System.out.println(10 >> 1);
-		System.out.println(5 >> 1);
-		// 1010 -> 10100(20) -> 101000(40)
-		System.out.println(10 << 1);
-		System.out.println(20 << 1);
-		System.out.println(-10 >> 1);
-		System.out.println(-10 >>> 1);
-		
-		// 복합 대입 연산자 : 산술, 비트 연산을 수행 후 대입까지 수행하는 연산자
-		int x = 0;
-		x = x + 10;
-		System.out.println(x);
-		
-		int y = 0;
-		y += 10;   // 1씩 증가하는 값은 ++y 사용하면 되지만, 2 이상씩 증가 감소
-		// 하는 연산에 대한 코드 작성할 때 많이 사용
-		System.out.println(y);
-		
-		// 삼항 연산자
-		// 조건식 ? 식1 : 식2;
-		//조건식이 참이면 식1 수행, 조건식이 거짓이면 식2 수행
-		int z = 8;
-		boolean b3;
-		
-		//   조건식       ?  식1  :  식2;
-		b3 = z % 2 == 0 ? true : false;
-		System.out.printf("정수 %d는 짝수가 %b\n", z, b3);
-		
-		// 삼항 연산 중첩
-		String s;
-		s = z < 10 ? z % 2 == 1 ? "z는 10보다 작고 홀수" : "z는 10보다 작고 짝수" : "z는 10초과";
-		System.out.println(s);
+		func1();
 	}
 
 }
