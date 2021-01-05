48fdbda74ce03d04c383ff1375b89219a2548a33
seo-dasom <74850449+seo-dasom@users.noreply.github.com>
2021-01-05 15:37:29 +0900

    kh_210105

--- Circle.java
+++ Circle.java
@@ -1,48 +1,32 @@
-package com.kh.practice.point.model.vo;
+package com.kh.test.shape.model.vo;
 
-public class Circle extends Point {
-	public static final double PI = 3.14;
+public class Circle {
+	public double PI = Math.PI;
 	private int radius;
 	
 	public Circle() {}
 	
-	public Circle(int x, int y, int radius) {
-		super(x, y);
+	public Circle(int radius) {
 		this.radius = radius;
 	}
-	
+
+	public double getPI() {
+		return PI;
+	}
+
+	public void setPI(double pI) {
+		PI = pI;
+	}
+
 	public int getRadius() {
 		return radius;
 	}
-	
+
 	public void setRadius(int radius) {
 		this.radius = radius;
 	}
 	
-	// 필수
-	// @Override : 실제로 오버라이드가 되는 메서드인지 검사
-	@Override
-	public int areaInt() {
-		return (int)PI * radius * radius;
-	}
-	
-	@Override
-	public int roundInt() {
-		return 2 * (int)PI * radius;
-	}
-	
-	@Override
-	public double areaDouble() {
-		return PI * radius * radius;
-	}
-	
-	@Override
-	public double roundDouble() {
-		return 2 * PI * radius;
-	}
-	
-	@Override
-	public String toString() {
-		return super.toString() + " , " + this.radius;
+	public void draw() {
+		System.out.println("반지름 " + radius + "cm인 원을 그립니다.");
 	}
 }
---
+++ ShapeMaker.java
@@ -0,0 +1,25 @@
+package com.kh.test.shape.controller;
+
+import com.kh.test.shape.model.vo.*;
+
+public class ShapeMaker {
+
+	public static void main(String[] args) {
+		Circle[] c = new Circle[3];
+		c[0] = new Circle(20);
+		c[1] = new Circle(30);
+		c[2] = new Circle(40);
+		RectAngle[] r = new RectAngle[2];
+		r[0] = new RectAngle(20, 10);
+		r[1] = new RectAngle(30, 20);
+		
+		for(int i = 0; i < c.length; i++) {
+			c[i].draw();
+		}
+		
+		for(int i = 0; i < r.length; i++) {
+			r[i].draw();
+		}
+	}
+
+}
